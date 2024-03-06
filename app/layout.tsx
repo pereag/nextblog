4805114e57ed357a344d05c172ca150a000f0771
pereag <perello.valentin@hotmail.fr>
2024-03-06 10:09:40 +0100

    feat: add routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <p>Header</p>
+        </div>
+        {children}
+        <div>
+          <p>Footer</p>
+        </div>
+      </body>
     </html>
   );
 }
