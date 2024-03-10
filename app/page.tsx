ce45bf10032fbc4a2197d5e219237ab76e4e4ffa
pereag <perello.valentin@hotmail.fr>
2024-03-10 14:14:24 +0100

    add Home banner with title and input

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,33 @@
 'use client';
+import PageContainer from '@/components/PageContainer';
 import { Button } from '@/components/ui/button';
 import { Eye } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
-  const router = useRouter();
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button variant={'outline'} onClick={() => router.push('/login')}>
-        Login
-      </Button>
-      <Button
-        variant={'outline'}
-        onClick={() => router.push('/categories/react')}
+    <PageContainer>
+      <div
+        className='aspect-square overflow-hidden rounded-lg bg-cover md:aspect-[2.4/1]'
+        style={{ backgroundImage: 'url(https://picsum.photos/1920/1080)' }}
       >
-        React
-      </Button>
-    </main>
+        <div className='flex-ol flex h-full w-full flex-col items-center justify-center'>
+          <div className='max-w-xs rounded-lg bg-secondary/80 p-4 sm:max-w-xl'>
+            <h1 className='text)3xl text-center font-bold sm:text-5xl'>
+              Becom a better react déveloper
+            </h1>
+            <Input
+              placeholder='Email'
+              className='mt-4 w-full py-6 text-xl dark:bg-white'
+            />
+            <Button size='lg' className='mt-4 w-full py-6 text-xl'>
+              Subscribe to our Newsletter
+            </Button>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
   );
 }
