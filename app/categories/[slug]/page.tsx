bbb578f74a0d3f268a9214ac6ddca2cdfe9c316a
pereag <perello.valentin@hotmail.fr>
2024-08-04 06:41:15 +0200

    chore: config for publication

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import PageContainer from '@/components/PageContainer';
 import PageTitle from '@/components/PageTitle';
-import { IPost } from '@/types';
 import React from 'react';
 
 type Props = {
@@ -8,15 +7,6 @@ type Props = {
     slug: string;
   };
 };
-
-export async function generateStaticParams() {
-  const res = await fetch(`${process.env.API_BASE_URL}posts`);
-  const posts = await res.json();
-
-  return posts.map((post: IPost) => ({
-    slug: post.slug,
-  }));
-}
 
 export default function CategoriesPage({ params }: Props) {
   const { slug } = params;
